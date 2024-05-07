652bd5be805d099cf94c646fcb2f03baabf91e63
honganh1253 <arayasakaopun0suy2@hotmail.com>
2024-05-08 00:53:36 +0700

    demo

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-function maximalSquare(matrix) {
-  if (matrix.length === 0) return 0;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  const dp = Array.from(Array(rows + 1), () => Array(cols + 1).fill(0));
-  let maxSquare = 0;
-  for (let i = 1; i <= rows; i++) {
-    for (let j = 1; j <= cols; j++) {
-      if (matrix[i - 1][j - 1] === "1") {
-        dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
-        maxSquare = Math.max(maxSquare, dp[i][j]);
-      }
-    }
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const count = Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    count[s.charCodeAt(i) - 97]++;
+    count[t.charCodeAt(i) - 97]--;
   }
-  return maxSquare * maxSquare;
+  return count.every((c) => c === 0);
 }
